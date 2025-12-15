50450675073db05ea5347c20fa10301d0934c75b
mayanksrivastava922 <catvideos999yt@gmail.com>
2025-12-15 19:31:39 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,29 @@
 import express from 'express'
-// const express = require('express')
 import dotenv from 'dotenv'
+import path from 'path'
+
 import authRoute from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+
 dotenv.config()
 
-const PORT = process.env.PORT
 const app = express()
+const __dirname = path.resolve()
+
+const PORT = process.env.PORT ||3000;
 
 app.use('/api/auth/',authRoute)
 app.use('/api/messages/',messageRoutes)
+
+//make ready for deployment
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  
+  app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)

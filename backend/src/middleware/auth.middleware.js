dabb3786887d32165b95e34c22dfbba985a6c933
mayanksrivastava922 <catvideos999yt@gmail.com>
2025-12-20 19:05:37 +0530

    update profile endpoint added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+
+export const protectRoute = async(req,res,next) =>{
+    try {
+        const token = req.cookies.jwt
+        if(!token) return res.status(401).json({message:"Unauthorized - No Token Provide"})
+        
+        const decoded = jwt.verify(token, ENV.JWT_SECRET)
+        if(!decoded) return res.status(401).json({message:"Unauthorized - Invalid Token"})
+        
+        const user = await User.findById(decoded.userID).select("-password");
+        if(!user) return res.status(404).json({message:"User Not Found"})
+        
+        req.user=user
+        next();
+    } catch (error) {
+        console.error("Error in ProtectRoute Middleware", error);
+        res.status(500).json({message:"Internal Server Error"})
+        
+    }
+}

99dec04ca99f4e79fd0170962b1c1f0f37848773
mayanksrivastava922 <catvideos999yt@gmail.com>
2025-12-21 12:06:23 +0530

    rate limit implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,17 @@
 import express from 'express'
-import { signup , login , logout , updateProfile} from '../controller/auth.controller.js'
+import { signup, login, logout, updateProfile } from '../controller/auth.controller.js'
 import { protectRoute } from '../middleware/auth.middleware.js'
+import { arcjetProtection } from '../middleware/arcjet.middleware.js'
+
 const router = express.Router()
 
-router.post('/signup',signup) // api - let test it from postman
+router.use(arcjetProtection);
+
+router.post('/signup', signup) // api - let test it from postman
 router.post('/login', login)
 router.post('/logout', logout)
 
-router.put("/update-profile", protectRoute,updateProfile)
-router.get("/check",protectRoute, (req,res)=>res.status(200).json(req.user))
+router.put("/update-profile", protectRoute, updateProfile)
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user))
 
 export default router
